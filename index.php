1ef8681048ff5df314392f979975e717bdb0ed33
jstockwin <jstockwin@gmail.com>
2016-04-14 15:55:39 +0100

    Update Script

--- index.php
+++ index.php
@@ -1,34 +1,39 @@
-
 <?php
-include '/../database.php';
-$conn = new myqli($host, $username, $password, "deployment");
+include '../database.php';
+$conn = new mysqli($host, $username, $password, "deployment");
 
 if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
 }
 
-if(isset($_REQUEST['payload'])){
-        $payload = json_decode(stripslashes($_REQUEST['payload']));
+
+
+
+if(isset($_POST['payload'])){
+        echo "Payload recieved\r\n";
+        $payload = json_decode(stripslashes($_POST['payload']));
         $repo = $payload->{'repository'}->{'name'};
         $commit = $payload->{'head_commit'}->{'message'};
         $time = $payload->{'head_commit'}->{'message'};
         $committer = $payload->{'head_commit'}->{'committer'}->{'username'};
-
         $sql = "REPLACE INTO deployments VALUES ('$repo', '$commit', '$time', '$committer')";
-        $conn->query($sql);
-}
-
-$result = $conn->query("SELECT * FROM deployments");
-if($result->num_rows >0){
-        echo    "<table>";
-        echo    "<tr>";
-        echo    "<td>Repository</td><td>Last Commit</td><td>Time</td><td>User</td>";
-        echo    "</tr>";
-        while($row = $result->fetch_assoc()){
-        echo "<tr><td>".$row['repo']."</td><td>".$row['commit']."</td><td>".$row['time']."</td><td>".$row['committer']."</td></tr>";
+        $result = $conn->query($sql);
+        if($result){
+                echo "Success";
+        }else{
+                echo "Something went wrong with SQL. Result was:\r\n";
+                echo $result;
+        }
+}else{
+        $result = $conn->query("SELECT * FROM deployments");
+        if($result->num_rows >0){
+                echo    "<table>";
+                echo    "<tr>";
+                echo    "<td>Repository</td><td>Last Commit</td><td>Time</td><td>User</td>";
+                echo    "</tr>";
+                while($row = $result->fetch_assoc()){
+                echo "<tr><td>".$row['repo']."</td><td>".$row['commit']."</td><td>".$row['time']."</td><td>".$row['committer']."</td></tr>";
+                }
         }
 }
-
-
 ?>
-
