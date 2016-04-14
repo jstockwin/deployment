7b718463137cff64609ae3e3a40422a73c257056
jstockwin <jstockwin@gmail.com>
2016-04-14 19:06:25 +0100

    Start deploying

--- index.php
+++ index.php
@@ -6,25 +6,26 @@ if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
 }
 
-
-
-
 if(isset($_POST['payload'])){
-        echo "Payload recieved\r\n";
+        echo "Payload received\r\n";
         $payload = json_decode(stripslashes($_POST['payload']));
         $repo = $payload->{'repository'}->{'name'};
         $commit = $payload->{'head_commit'}->{'message'};
         $time = $payload->{'head_commit'}->{'message'};
         $committer = $payload->{'head_commit'}->{'committer'}->{'username'};
-        $sql = "REPLACE INTO deployments VALUES ('$repo', '$commit', '$time', '$committer')";
+        $sql = "REPLACE INTO deployments VALUES ('$repo', '$commit', '$time', '$committer', 'awaiting deployment')";
         $result = $conn->query($sql);
         if($result){
-                echo "Success";
+                echo "Successfully updated SQL";
         }else{
                 echo "Something went wrong with SQL. Result was:\r\n";
                 echo $result;
         }
+                gitPull($conn, $repo, true);
 }else{
+        if(isset($_GET['repo'])){
+                gitPull($conn, $_GET['repo']);
+        }
         $result = $conn->query("SELECT * FROM deployments");
         if($result->num_rows >0){
                 echo    "<table>";
@@ -36,4 +37,33 @@ if(isset($_POST['payload'])){
                 }
         }
 }
+
+
+function gitPull($conn, $repo, $out=false){
+        $managing = TRUE;
+        switch($repo) {
+                case "Film-Night":
+                        $dir = "/var/www/films.jakestockwin.co.uk/public_html";
+                        break;
+                case "Deployment":
+                        $dir = "/var/www/deployment.jakestockwin.co.uk/public_html";
+                        break;
+                default:
+                        $managing = FALSE;
+        }
+
+        if($managing){
+			    $sql = "UPDATE deployments SET status = 'Deployed using git pull' WHERE repo = '$repo'";
+                $conn->query($sql);
+                $output = shell_exec("cd $dir && git pull");
+                if($out){
+                        echo $output;
+                }
+        }else{
+                //deployment is not hosted by this server.
+                $sql = "UPDATE deployments SET status = 'Deployment not handled by this server' WHERE repo = '$repo'";
+                $conn->query($sql);
+        }
+}
+
 ?>
