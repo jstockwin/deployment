f6afcf8cd73dd416eb5db445789efe7e401510a0
jstockwin <jstockwin@gmail.com>
2016-04-14 15:43:26 +0100

    Bug fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
 include '/../database.php';
-$conn = new myqli($host, $username, $password, "deployments");
+$conn = new myqli($host, $username, $password, "deployment");
 
 if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
@@ -14,11 +14,11 @@ if(isset($_REQUEST['payload'])){
         $time = $payload->{'head_commit'}->{'message'};
         $committer = $payload->{'head_commit'}->{'committer'}->{'username'};
 
-        $sql = "REPLACE INTO deployment VALUES ('$repo', '$commit', '$time', '$committer')";
+        $sql = "REPLACE INTO deployments VALUES ('$repo', '$commit', '$time', '$committer')";
         $conn->query($sql);
 }
 
-$result = $conn->query("SELECT * FROM deployment");
+$result = $conn->query("SELECT * FROM deployments");
 if($result->num_rows >0){
         echo    "<table>";
         echo    "<tr>";
